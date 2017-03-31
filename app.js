a9ba223b4bb59d8dd5f9f7d45ff7ad25647cec10
Daniel Zambrano <daniel@netengine.com.au>
2017-03-31 13:48:23 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var xhr;
+
+var Communication = {
+  initialize: function(){
+    var _orgAjax = jQuery.ajaxSettings.xhr;
+
+    jQuery.ajaxSettings.xhr = function () {
+      xhr = _orgAjax();
+      return xhr;
+    };
+  },
+
+  send : function(arguments){
+    window.url = "http://timefit.outfit.io/projects/2790/selections/78997/dynamic_export"+
+              "?&document_id=26812&access_token=U1WqGTJDHE4q8MsGSj54S8Bw&" +arguments;
+
+    $('#image_id').attr('src', url)
+  }
+};
+
+var Forms = {
+  initialize : function(){
+    var selectTimeZone = document.querySelector('#timeZoneList');
+    selectTimeZone.innerHTML = moment.tz.names().map((n) => "<option>"+n+"</option>")
+  },
+  generate : function(e){
+    e.preventDefault();
+    Communication.send(unescape($('form').serialize()));
+  }
+}
+
+$(function(){
+  Communication.initialize();
+  Forms.initialize();
+  document.querySelector('#generate').addEventListener("click", Forms.generate);
+})
